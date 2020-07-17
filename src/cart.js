f91767001efc11ac5604bc4656fc0e84e30af087
Huzaifa Habib <huzaifa.dj@gmail.com>
2020-07-17 16:55:17 +0500

    Empty card messgae addition

--- src/cart.js
+++ src/cart.js
@@ -8,7 +8,7 @@ export const Cart = () => {
 
     // const[cart , setCart] = useContext(CartContext);
     // let { transactions, addTransaction, deleteTransaction } = useContext(CartContext);
-    let { transactions, deleteTransaction} = useContext(CartContext);
+    let { transactions, deleteTransaction } = useContext(CartContext);
 
     const total = () => {
         var totalamount = 0;
@@ -19,14 +19,27 @@ export const Cart = () => {
         return totalamount;
     }
 
-    const handleDelete = (ind)=> {
-        deleteTransaction({id:ind})
+    const handleDelete = (ind) => {
+        deleteTransaction({ id: ind })
         console.log(ind)
     }
 
-    const cartDetail = transactions.map((item, ind) => {
-        return <li>Product {ind + 1} Price : {item.amount} <button onClick={()=>{handleDelete(ind)}}>Remove</button></li>
-    })
+
+    const RenderCartDetail = () => {
+
+        const cartDetail = transactions.map((item, ind) => {
+            return <li>Product {ind + 1} Price : {item.amount} <button onClick={() => { handleDelete(ind) }}>Remove</button></li>
+        })
+
+        if (transactions.length === 0) {
+            return <h6>Your Cart is Empty</h6>
+        }
+        else {
+            return cartDetail;
+        }
+
+    }
+
 
     return (
         <div>
@@ -36,7 +49,7 @@ export const Cart = () => {
             <div>
                 <h3>Cart Item Details</h3>
                 <ul>
-                    {cartDetail}
+                    <RenderCartDetail />
                 </ul>
             </div>
         </div>
